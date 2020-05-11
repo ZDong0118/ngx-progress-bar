cbb97093dcd0600a371b6866283cf3c764f2d9ed
Zecheng Dong <zecheng.dong@gmail.com>
2020-05-10 23:41:30 -0400

    Fix build error

--- projects/ngx-progress-bar/src/public-api.ts
+++ projects/ngx-progress-bar/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-progress-bar
  */
 
-export * from './lib/ngx-progress-bar.service';
 export * from './lib/ngx-progress-bar.component';
 export * from './lib/ngx-progress-bar.module';
